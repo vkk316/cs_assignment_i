e71e6acb13a08a3bd9ef55784ba1a9c554aef23a
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2022-01-05 11:29:58 +0700

    simple_yesssss

--- client.js
+++ client.js
@@ -9,7 +9,7 @@ client.connect(PORT, HOST, function () {
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     // Write a message to the socket as soon as the client is connected,
     //the server will receive it as message from the client 
-    client.write('I am Chuck Norris!');
+    client.write('1');
 
 });
 
@@ -19,7 +19,7 @@ client.on('data', function (data) {
 
     console.log('DATA: ' + data);
     // Close the client socket completely
-    client.destroy();
+    //client.destroy();
 
 });
 
